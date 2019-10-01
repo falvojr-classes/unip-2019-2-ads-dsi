cd6175fef023981fff0aaeca1f5c1a7b0e9e1627
Venilton FalvoJr <falvojr@gmail.com>
2019-09-30 23:54:05 -0300

    Aula 30/09

--- 4. JavaScript/exercicio03/js/cliente.js
+++ 4. JavaScript/exercicio03/js/cliente.js
@@ -1,16 +1,55 @@
 async function buscarCep() {
     try {
-        const cep = document.getElementById('cep').value;
-        const resp = await fetch(`https://viacep.com.br/ws/${cep}/json/`, { method: 'GET' });
-        if(resp.ok) {
-            const dados = await resp.json();
-            if(dados.logradouro) {
-                document.getElementById('logradouro').value = `${dados.logradouro}, ${dados.bairro}`;
-                document.getElementById('complemento').value = dados.complemento;
+        const cep = getById('cep').value;
+        if (cep) {
+            const resp = await fetch(`https://viacep.com.br/ws/${cep}/json/`, { 
+                method: 'GET' 
+            });
+            if(resp.ok) {
+                const dados = await resp.json();
+                if(dados.logradouro) {
+                    getById('logradouro').value = `${dados.logradouro}, ${dados.bairro}`;
+                    getById('complemento').value = dados.complemento;
+                }
             }
         }
     } catch(erro) {
         console.log(erro);
         alert("Erro inesperado!");
     }
+}
+
+async function cadastrarUsuario() {
+    try {
+        const usuario = {};
+        usuario.nome = getById('nome').value;
+        usuario.cpf = getById('cpf').value;
+        usuario.email = getById('email').value;
+        usuario.senha = getById('senha').value;
+        usuario.telefone = getById('telefone').value;
+        usuario.cep = getById('cep').value;
+        usuario.logradouro = getById('logradouro').value;
+        usuario.complemento = getById('complemento').value;
+        usuario.tipo = 'CLIENTE';
+
+        const resp = await fetch(`http://localhost:8080/usuarios`, { 
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify(usuario)
+        });
+        if(resp.ok) {
+            alert('Usuário cadastrado com sucesso!');
+        } else {
+            alert("Erro inesperado na API!");
+        }
+    } catch(erro) {
+        console.log(erro);
+        alert("Erro inesperado!");
+    }
+}
+
+function getById(id) {
+    return document.getElementById(id);
 }
